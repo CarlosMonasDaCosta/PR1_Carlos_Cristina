65235c647dd5df6a62e4efd079c9f6bb7844d735
CarlosMonasDaCosta <99674714+CarlosMonasDaCosta@users.noreply.github.com>
2022-05-15 15:57:58 +0200

    Update SignUp.kt
    SharedPreference funcional

--- app/src/main/java/com/example/pr1_carlos_cristina/SignUp.kt
+++ app/src/main/java/com/example/pr1_carlos_cristina/SignUp.kt
@@ -1,12 +1,13 @@
 package com.example.pr1_carlos_cristina
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.UserHandle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 
 
@@ -16,15 +17,63 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        
+        val UsuariText: EditText
+        UsuariText = findViewById(R.id.editTextTextPersonName)
+
+        val contrasenyaText: EditText
+        contrasenyaText = findViewById(R.id.editTextTextPersonName7)
+
+        var usuari:String? = null
+        var contrasenya:String? = null
+
+
+        val ErrorMessage: TextView
+        ErrorMessage = findViewById(R.id.ErrorMessage)
+
+
         findViewById<Button>(R.id.sign_up)?.setOnClickListener{
+
+            //Escriptura del fitxer Registre.xml
+            var preferences: SharedPreferences
+            preferences = getSharedPreferences("Registre", Context.MODE_PRIVATE)
+
+            var editor: SharedPreferences.Editor
+            editor = preferences.edit()
+
+            usuari = UsuariText.getText().toString()
+            contrasenya = contrasenyaText.getText().toString()
+
+            editor.putString("user",usuari)
+            editor.putString("password",contrasenya)
+
+            editor.commit()
+
             val intent: Intent = Intent(this,SignIn::class.java)
             startActivity(intent)
         }
 
         findViewById<Button>(R.id.sign_in)?.setOnClickListener{
-            val intent: Intent = Intent(this,SignIn::class.java)
-            startActivity(intent)
+
+            //Lectura del fitxer Registre.xml
+            var preferences: SharedPreferences
+            preferences = getSharedPreferences("Registre", Context.MODE_PRIVATE)
+
+
+            var user: String? = preferences.getString("user","No DATA FOUND")
+            var password: String? = preferences.getString("password","No DATA FOUND")
+
+            usuari = UsuariText.getText().toString()
+            contrasenya = contrasenyaText.getText().toString()
+
+            if(user.equals(usuari) and password.equals(contrasenya)){
+                val intent: Intent = Intent(this,SignIn::class.java)
+                startActivity(intent)
+            }
+            else{
+                ErrorMessage.setText("El usuari no ha sigut registrat")
+            }
+
+
         }
 
     }
